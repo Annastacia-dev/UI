dc7bf682b3cfb0991b73f83b2f03739c5214f444
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-04-09 09:54:06 +0300

    crumbly: test adding custom font

---
+++ crumbly/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+ <div class="bg-gray-50 grid sm:grid-cols-2 grid-cols-1 gap-5 pt-20 px-6 min-h-screen">
+    <div class="flex flex-col gap-5">
+      <h1 class="text-5xl font-bold  capitalize leading-tight font-asgard">
+         Handmade,
+         <br />
+         with an extra
+         <br />
+         pinch of love
+      </h1>
+    </div>
+ </div>
+</template>
