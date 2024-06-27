568278d96378f38f929a1e4cdd19d6ed4714efaf
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-27 20:09:16 +0300

    initialie cookbook ui

---
+++ cookbook/src/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      Cookbook
+    </div>
+  )
+}
+
+export default LandingPage
