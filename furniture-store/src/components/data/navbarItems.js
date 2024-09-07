431dcfd2a452754d24733ffc8744934d675c375d
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-09-07 21:13:30 +0300

    plenish: navbar submenu

---
+++ furniture-store/src/components/data/navbarItems.js
@@ -0,0 +1,71 @@
+export const menuItems = [
+  {
+    title: "office",
+    items: [
+      { title: "executive desks", path: "/office/executive-desks" },
+      { title: "reception counters", path: "/office/reception-counters" },
+      { title: "conference tables", path: "/office/conference-tables" },
+      { title: "storage cabinets", path: "/office/storage-cabinets" },
+      { title: "office sofa", path: "/office/office-sofa" },
+      { title: "office safes", path: "/office/office-safes" },
+      { title: "coffee tables", path: "/office/coffee-tables" },
+      { title: "occasional", path: "/office/occasional" },
+      { title: "breakout", path: "/office/breakout" },
+      { title: "training desks & chairs", path: "/office/workstations" },
+      {
+        title: "workstations",
+        path: "/office/workstations",
+        items: [
+          { title: "2 way workstations", path: "/office/workstations" },
+          { title: "4 way workstations", path: "/office/workstations" },
+          { title: "straight desk", path: "/office/workstations" },
+        ],
+      },
+      {
+        title: "office chairs",
+        path: "/office/office-chairs",
+        items: [
+          { title: "high back chairs", path: "/office/high-back-chairs" },
+          { title: "low back chairs", path: "/office/low-back-chairs" },
+          { title: "visitors back chairs", path: "/office/visitors-chairs" },
+        ],
+      },
+    ],
+  },
+  { title: "home office", path: "/home-office" },
+  {
+    title: "living room",
+    items: [
+      {
+        title: "fabric sofas",
+        items: [
+          {
+            title: "corner sofas",
+            path: "/living-room/fabric-sofas/corner-sofas",
+          },
+          {
+            title: "stationery sofas",
+            path: "/living-room/fabric-sofas/stationery-sofas",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "dining",
+    items: [
+      { title: "dining sets", path: "/dining/dining-sets" },
+      { title: "dining chairs", path: "/dining/dining-chairs" },
+    ],
+  },
+  { title: "storage", path: "/storage" },
+  { title: "bedroom", path: "/bedroom" },
+  { title: "mattress & pillows", path: "/mattress-and-pillows" },
+  { title: "kids room", path: "/kids-room" },
+  { title: "home decor", path: "/home-decor" },
+  {
+    title: "outdoor",
+    path: "/outdoor",
+    items: [{ title: "outdoor-dining", path: "" }],
+  },
+];
--- furniture-store/src/components/layouts/Navbar.jsx
+++ furniture-store/src/components/layouts/Navbar.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
 import {
   FaStore,
   FaUser,
@@ -7,43 +6,26 @@ import {
   FaSquareFacebook,
   FaInstagram,
   FaYoutube,
+  FaArrowRightLong,
 } from "react-icons/fa6";
 
-const menuItems = [
-  {
-    title: "office",
-    items: [{ title: "executive desks" }, { title: "reception counters" }],
-  },
-  {
-    title: "living room",
-    items: [
-      {
-        title: "fabric sofas",
-        items: [{ title: "corner sofas" }, { title: "stationery sofas" }],
-      },
-    ],
-  },
-  {
-    title: "dining",
-    items: [{ title: "dining sets" }, { title: "dining chairs" }],
-  },
-];
+import { menuItems } from "../data/navbarItems";
 
 const storeIcons = [
   {
     title: "login/register",
     icon: <FaUser />,
-    path: "",
+    path: "/login",
   },
   {
     title: "cart",
     icon: <FaCartShopping />,
-    path: "",
+    path: "/cart",
   },
   {
     title: "our stores",
     icon: <FaStore />,
-    path: "",
+    path: "/stores",
   },
 ];
 
@@ -51,23 +33,37 @@ const socials = [
   {
     title: "facebook",
     icon: <FaSquareFacebook />,
-    path: "",
+    path: "https://facebook.com",
   },
   {
     title: "instagram",
     icon: <FaInstagram />,
-    path: "",
+    path: "https://instagram.com",
   },
   {
     title: "youtube",
     icon: <FaYoutube />,
-    path: "",
+    path: "https://youtube.com",
   },
 ];
 
 const Navbar = () => {
-  const [hoveredStore, setHoveredStore] = useState(null);
   const [hoveredMenuItem, setHoveredMenuItem] = useState(null);
+  const hoverTimeoutRef = useRef(null);
+
+  const handleMouseEnter = (itemTitle, index) => {
+    if (hoverTimeoutRef.current) {
+      clearTimeout(hoverTimeoutRef.current);
+    }
+    setHoveredMenuItem(itemTitle);
+    adjustSubmenuPosition(index);
+  };
+
+  const handleMouseLeave = () => {
+    hoverTimeoutRef.current = setTimeout(() => {
+      setHoveredMenuItem(null);
+    }, 100);
+  };
 
   return (
     <>
@@ -80,52 +76,77 @@ const Navbar = () => {
 
         <ul className="flex gap-6 items-center">
           {storeIcons.map((store, index) => (
-            <div
-              key={index}
-              className="relative"
-              onMouseEnter={() => setHoveredStore(store.title)}
-              onMouseLeave={() => setHoveredStore(null)}
-            >
+            <div key={index} className="relative">
               <a className="text-lg" href={store.path}>
                 {store.icon}
               </a>
-              {hoveredStore === store.title && (
-                <p className="absolute top-full mt-1 p-2 text-sm capitalize font-semibold bg-black text-white border rounded w-40 text-center">
-                  {store.title}
-                </p>
-              )}
             </div>
           ))}
         </ul>
 
-        <ul className="flex gap-6 items-center text-sm">
+        <ul className="flex gap-4 items-center text-sm">
           <a href="mailto:sales@plenish.com">sales@plenish.com</a>
           <span>|</span>
           <a href="tel:076837249">+254 768 372 439</a>
           <span>|</span>
-          {socials.map((social) => (
-            <a href={social.path} target="_blank" className="text-lg">
+          {socials.map((social, index) => (
+            <a
+              key={index}
+              href={social.path}
+              target="_blank"
+              className="text-lg"
+            >
               {social.icon}
             </a>
           ))}
         </ul>
       </nav>
 
-      <nav className="w-full flex justify-between items-center px-7 py-1 border border-black">
+      <nav className="w-full flex justify-between items-center px-7 py-2 border border-black">
         <ul className="flex items-center gap-3 capitalize font-medium text-sm">
-          {menuItems.map((item) => (
+          {menuItems.map((item, menuIndex) => (
             <div
-              className="relative"
-              onMouseEnter={() => setHoveredMenuItem(item.title)}
-              onMouseLeave={() => setHoveredMenuItem(null)}
+              key={menuIndex}
+              className="relative group"
+              onMouseEnter={() => handleMouseEnter(item.title, menuIndex)}
+              onMouseLeave={handleMouseLeave}
+              id={`menu-item-${menuIndex}`}
             >
-              <li className="hover:bg-black hover:text-white px-3 py-2 cursor-pointer rounded relative">
+              <a
+                href={item.path}
+                className={`${item.items ? "hover:bg-black hover:text-white" : "hover:underline hover:font-semibold"} px-3 py-2 cursor-pointer rounded`}
+              >
                 {item.title}
-              </li>
-              {hoveredMenuItem == item.title && (
-                <div className="border border-black textblack  rounded px-4 py-3 grid grid-cols-6 gap-5 absolute min-w-[80vw] top-12">
-                  {item.items.map((subItem) => (
-                    <p>{subItem.title}</p>
+              </a>
+
+              {item?.items && hoveredMenuItem === item.title && (
+                <div
+                  id={`submenu-${menuIndex}`}
+                  className={`absolute top-8 mt-2 border border-black bg-white text-black rounded px-4 py-3 grid grid-cols-3 gap-2 min-w-[50vw] ${menuIndex > 5 ? "right-0" : ""}`}
+                  onMouseEnter={() => handleMouseEnter(item.title, index)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  {item.items.map((subItem, subIndex) => (
+                    <div className="flex flex-col gap-2" key={subIndex}>
+                      <a
+                        href={subItem.path}
+                        className="hover:underline hover:font-semibold"
+                      >
+                        {subItem.title}
+                      </a>
+                      <div className="px-1 text-gray-700 flex flex-col gap-1">
+                        {subItem?.items &&
+                          subItem.items.map((innerItem, innerIndex) => (
+                            <a
+                              key={innerIndex}
+                              href={innerItem.path}
+                              className="hover:underline hover:font-semibold flex items-center gap-1"
+                            >
+                              {innerItem.title} <FaArrowRightLong />
+                            </a>
+                          ))}
+                      </div>
+                    </div>
                   ))}
                 </div>
               )}
