304cf8d51d181d08f14f4afc5d178510a3e7ac45
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-09-11 12:26:27 +0300

    plenish: style: switch header bg gradient colors

--- furniture-store/src/components/Header.jsx
+++ furniture-store/src/components/Header.jsx
@@ -20,9 +20,9 @@ const Header = () => {
       className="min-h-screen bg-cover bg-fixed bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
     >
-      <div className="min-h-screen bg-gradient-to-r from-black via-black/80 to-transparent text-white pt-32 pb-12 px-10">
+      <div className="min-h-screen md:bg-gradient-to-r bg-gradient-to-b from-gray-50 via-gray-50/80 to-transparent  pt-32 pb-12 px-10">
         <div className="md:w-1/2 flex flex-col gap-8">
-          <h1 className="text-7xl font-semibold">
+          <h1 className="md:text-7xl text-5xl font-semibold">
             where comfort <br /> meets style.
           </h1>
           <p className="text-sm leading-6 md:w-3/4">
@@ -30,15 +30,15 @@ const Header = () => {
             transform your home into a sanctuary. From elegant living spaces to
             cozy corners, our furniture is built to inspire and last.
           </p>
-          <div className="relative w-3/4">
+          <div className="relative md:w-3/4">
             <input
-              className="w-3/4 px-4 py-2 rounded-full bg-transparent border mt-4 placeholder:text-white"
+              className="md:w-3/4 w-11/12 px-4 py-2 rounded-full bg-transparent border border-black mt-4 placeholder:text-black"
               placeholder="search"
             />
-            <IoSearch className="absolute text-2xl right-32 top-6" />
+            <IoSearch className="absolute text-2xl md:right-32 right-10 top-6" />
           </div>
 
-          <div className="grid grid-cols-2 gap-4 mt-5">
+          <div className="grid md:grid-cols-2 gap-4 mt-5">
             <a href="/dining" className="relative">
               <img src="/dining.webp" className="rounded hover:scale-105" />
               <p className="flex items-center gap-1 absolute top-2 left-3 text-black capitalize font-semibold text-sm">
